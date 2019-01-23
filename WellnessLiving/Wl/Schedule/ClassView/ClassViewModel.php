63299fcfd017cefd8454f0cfe91f2ed99af8f5c1
Gromakov <gromakov94@gmail.com>
2019-01-23 19:07:59 +0200

    wl-16239: SPA. Adapt new location search for custom applications. +review WL

--- WellnessLiving/Wl/Schedule/ClassView/ClassViewModel.php
+++ WellnessLiving/Wl/Schedule/ClassView/ClassViewModel.php
@@ -30,6 +30,31 @@ class ClassViewModel extends WlModelAbstract
   public $a_location = null;
 
   /**
+   * List of sessions to get information for. Every element has next keys:
+   * <dl><dt>string <var>dt_date</var></dt><dd>Date/time of the session in UTC.</dd>
+   * <dt>string <var>k_class_period</var></dt><dd>Session primary key in {@link RsClassPeriodSql} table.</dd></dl>
+   * <tt>null</tt> for mode of single session.
+   *
+   * @get get
+   * @var array[]|null
+   */
+  public $a_session_request = null;
+
+  /**
+   * List of sessions information received in a multiple session mode.
+   * Every element has next keys:
+   * <dl><dt>array <var>a_class</var></dt><dd>Class information.</dd>
+   * <dt>array <var>a_location</var></dt><dd>Location information.</dd>
+   * <dt>array <var>a_staff</var></dt><dd>Staff member information.</dd>
+   * <dt>array <var>dt_date</var></dt><dd>Session date/time in UTC.</dd>
+   * <dt>array <var>k_class_period</var></dt><dd>Session key.</dd></dl>
+   *
+   * @get result
+   * @var array[]|null
+   */
+  public $a_session_result = null;
+
+  /**
    * Staff list data.
    *
    * <tt>null</tt> if data is not loaded yet.
