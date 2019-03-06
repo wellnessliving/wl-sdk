302e03d0e2c299679f9441b9b385273348e8a9cf
GreggHenry02 <gregghenry+github@gmail.com>
2019-03-06 12:34:20 -0500

    wl-16846: Website Development +review WL-18123 * Added endpoint to get store categories. * Added endpoint to get list of store items.

---
+++ WellnessLiving/Wl/Catalog/StaffApp/CatalogList/CatalogListModel.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace WellnessLiving\Wl\Catalog\StaffApp\CatalogList;
+
+use WellnessLiving\WlModelAbstract;
+
+/**
+ * List of products in selected shop category for staff application.
+ */
+class CatalogListModel extends WlModelAbstract
+{
+  /**
+   * Products in the online store category.
+   *
+   * Every element has the next fields:
+   * <dl>
+   *   <dt>string <tt>a_shop_category</tt></dt><dd>List of online store category IDs.</dd>
+   *   <dt>string <tt>text_description</tt></dt><dd>Category description.</dd>
+   *   <dt>string <tt>text_title</tt></dt><dd>Category title.</dd>
+   * </dl>
+   *
+   * @get result
+   * @var array
+   */
+  public $a_shop_product = [];
+
+  /**
+   * Whether staff has access to the Client Add page or not.
+   *
+   * @get result
+   * @type {Boolean}
+   */
+  public $can_add = false;
+
+  /**
+   * Whether currently selected user is a new client and has access to introductory offers.
+   *
+   * @var bool
+   */
+  public $is_customer_new = false;
+
+  /**
+   * ID of the business to get categories for.
+   *
+   * @get get
+   * @var string
+   */
+  public $k_business = '0';
+
+  /**
+   * Location ID.
+   * Can affect the list of displayed products.
+   *
+   * @get get
+   * @var string
+   */
+  public $k_location = '0';
+
+  /**
+   * Client's login type.
+   *
+   * <tt>null</tt> means not set, or user is guest ('walk-in' option).
+   *
+   * @var string|null
+   */
+  public $k_login_type = null;
+
+  /**
+   * Visit ID to pay for.
+   *
+   * @get get
+   * @var string
+   */
+  public $k_visit = '';
+}
+
+?>
