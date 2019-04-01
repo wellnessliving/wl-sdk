f590f291d030c975f7d27b4b38764e3689139344
GreggHenry02 <gregghenry+github@gmail.com>
2019-04-01 09:29:36 -0400

    wl-17616: Update PHP 5.5 SDK +review WL-18379 * Added missing SDK 7 endpoint

---
+++ WellnessLiving/Wl/Profile/Setting/SettingModel.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace WellnessLiving\Wl\Profile\Setting;
+
+use WellnessLiving\WlModelAbstract;
+
+/**
+ * Retrieves a list of user settings.
+ */
+class SettingModel extends WlModelAbstract
+{
+  /**
+   * Whether 'Booking Confirmation Email' group is enabled.
+   *
+   * @get result
+   * @post post
+   * @var bool
+   */
+  public $is_email_booking_confirmation = 1;
+
+  /**
+   * Whether 'Booking Reminder Email' group is enabled.
+   *
+   * @get result
+   * @post post
+   * @var bool
+   */
+  public $is_email_booking_reminder = 1;
+
+  /**
+   * Whether 'Company News Email' group is enabled.
+   *
+   * @get result
+   * @post post
+   * @var bool
+   */
+  public $is_email_news = 1;
+
+  /**
+   * Whether all emails enabled.
+   *
+   * @get result
+   * @post post
+   * @var bool
+   */
+  public $is_email_notification = 1;
+
+  /**
+   * Whether 'Review Request Email' group is enabled.
+   *
+   * @get result
+   * @post post
+   * @var bool
+   */
+  public $is_email_review = 1;
+
+  /**
+   * Whether all Push is enabled.
+   *
+   * @get result
+   * @post post
+   * @var bool
+   */
+  public $is_push_notification = 1;
+
+  /**
+   * Whether all SMS is enabled.
+   *
+   * @get result
+   * @post post
+   * @var bool
+   */
+  public $is_sms_notification = 1;
+
+  /**
+   * ID of a business to show information for.
+   *
+   * <tt>null</tt> if not set yet.
+   *
+   * @get get
+   * @post get
+   * @var string
+   */
+  public $k_business = null;
+
+  /**
+   * ID of a user to show information for.
+   *
+   * <tt>null</tt> if not set yet.
+   *
+   * @get get
+   * @post get
+   * @var string
+   */
+  public $uid = null;
+}
+
+?>
