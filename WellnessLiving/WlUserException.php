842c5e90cd0b5a98aca4a6427f74747f41ac24b2
yuri-popoff <popoffyuri@gmail.com>
2019-06-19 20:55:45 +0300

    Improve error handling.

--- WellnessLiving/WlUserException.php
+++ WellnessLiving/WlUserException.php
@@ -55,6 +55,21 @@ class WlUserException extends \Exception
   private $s_class;
 
   /**
+   * Error code, if provided.
+   *
+   * Copy of the first argument of {@link \WellnessLiving\WlUserException::__construct()} if this error does not
+   * originate from API request.
+   *
+   * If error originates from API request, this code contains value returned by
+   * {@link \WellnessLiving\WlUserException::sid()} by default (for error with index 0).
+   *
+   * <tt>null</tt> if error code was not provided.
+   *
+   * @var string|null
+   */
+  private $s_code='';
+
+  /**
    * Constructs a user-level error.
    *
    * @param string $s_code Error code.
@@ -64,7 +79,31 @@ class WlUserException extends \Exception
   public function __construct($s_code = '',$s_message = '',array $a_data=[])
   {
     $this->a_data=$a_data;
+    $this->s_code=$s_code;
     parent::__construct($s_message,0);
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public function __toString()
+  {
+    $text_exception='';
+    if($this->s_code)
+      $text_exception.='['.$this->s_code.'] ';
+    $text_exception.=get_class($this);
+    if($this->s_class)
+      $text_exception.=' / '.$this->s_class;
+    $text_exception.=' is thrown with error message: '.$this->getMessage();
+    if($this->a_result)
+      $text_exception.="\r\n".var_export($this->a_result,true);
+    elseif($this->a_error) // If Both, a_result and a_error specified, it is likely that a_result contains a_error inside.
+      $text_exception.="\r\n".var_export($this->a_error,true);
+
+    if($this->a_data)
+      $text_exception.="\r\n".var_export($this->a_data,true);
+
+    return $text_exception;
   }
 
   /**
