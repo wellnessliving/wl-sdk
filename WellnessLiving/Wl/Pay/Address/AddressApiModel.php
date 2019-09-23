5b024f869e1bed0d67c26322a806e885bbb2c629
Smakhtin Serhii <serhii.smakhtin@wellnessliving.com>
2019-09-23 10:30:59 +0300

    Add model WellnessLiving/Wl/Pay/Address/AddressApiModel.php

---
+++ WellnessLiving/Wl/Pay/Address/AddressApiModel.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace WellnessLiving\Wl\Pay\Address;
+
+use WellnessLiving\WlModelAbstract;
+
+class AddressApiModel extends WlModelAbstract
+{
+  /**
+   * Object result
+   *
+   * @get result
+   * @var null
+   */
+  public $a_pay_address = null;
+
+  /**
+   * ID of payment owner type.
+   * One of {@link RsPayOwnerSid} constants.
+   *
+   * @get get
+   * @var int
+   */
+  public $id_pay_owner = 0;
+
+  /**
+   * Primary key of a payment owner.
+   * May be business of user depending on a {@link \Wl\Pay\Address\AddressApi::$id_pay_owner} value.
+   *
+   * @get get
+   * @var string
+   */
+  public $k_id = '0';
+}
