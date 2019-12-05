a8349e8c9a8011ef7a94544220035b3d14ab2c58
Vladislav Kobzev <kp42@ya.ru>
2019-12-05 11:48:54 +0300

    wl-22324: Add an attribute  repeat every +review WL-21899

---
+++ WellnessLiving/Core/a/ADurationSid.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace WellnessLiving\Core\a;
+
+/**
+ * Class for manage time intervals.
+ *
+ * Last ID: 8.
+ */
+class ADurationSid
+{
+  /**
+   * Days.
+   */
+  const DAY = 4;
+
+  /**
+   * Hours.
+   */
+  const HOUR = 3;
+
+  /**
+   * Minutes.
+   */
+  const MINUTE = 2;
+
+  /**
+   * Months.
+   */
+  const MONTH = 5;
+
+  /**
+   * Seconds.
+   */
+  const SECOND = 1;
+
+  /**
+   * Weeks (7 days).
+   */
+  const WEEK = 7;
+
+  /**
+   * Foursome of weeks (28 days).
+   */
+  const WEEK4 = 8;
+
+  /**
+   * Years.
+   */
+  const YEAR = 6;
+}
+
+?>
