d76c4c840a90223144636986cd4a3ffb4c11aa5c
AndriiVMWork <andriimuzika@gmail.com>
2019-12-18 20:07:13 +0200

    wl-22483: API. Get a list of active Wellness Programs * Docs to field of array of programs. +review WL-22149

--- WellnessLiving/Wl/Insurance/Catalog/WellnessProgramModel.php
+++ WellnessLiving/Wl/Insurance/Catalog/WellnessProgramModel.php
@@ -5,17 +5,44 @@ namespace WellnessLiving\Wl\Insurance\Catalog;
 use WellnessLiving\WlModelAbstract;
 
 /**
- * Retrieves a list of programs and partner info.
+ * Retrieves a list of programs info.
  */
 class WellnessProgramModel extends WlModelAbstract
 {
   /**
-   * Array of Programs.
+   * Array of Programs. Every element has next keys:
    *
-   * @get get
-   * @var object|null
+   * <dl>
+   *   <dt>
+   *      string <var>m_price</var>
+   *   </dt>
+   *   <dd>
+   *     Purchase option price.
+   *   </dd>
+   *   <dt>
+   *     string <var>text_insurance_organization</var>
+   *   </dt>
+   *   <dd>
+   *     Insurance organization name.
+   *   </dd>
+   *   <dt>
+   *     string <var>text_partner</var>
+   *   </dt>
+   *   <dd>
+   *     "Wellness Program" partner name.
+   *   </dd>
+   *   <dt>
+   *     string <var>text_program</var>
+   *   </dt>
+   *   <dd>
+   *     "Wellness Program" name.
+   *   </dd>
+   * </dl>
+   *
+   * @get result
+   * @var array[]
    */
-  public $a_insure_program;
+  public $a_wellness_program;
 }
 
 ?>
