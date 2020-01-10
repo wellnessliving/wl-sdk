39ddac5de965c51763b5fdb26a035fb0f46a9211
Smakhtin Serhii <serhii.smakhtin@wellnessliving.com>
2020-01-10 17:56:44 +0200

    wl-22769: Add to SDK model for \Wl\Profile\Term\TermApi +review WL-22504

--- WellnessLiving/Wl/Profile/Term/TermModel.php
+++ WellnessLiving/Wl/Profile/Term/TermModel.php
@@ -24,6 +24,46 @@ class TermModel extends WlModelAbstract
    * @var string
    */
   public $uid = '0';
+
+  /**
+   * Date/time of the waiver confirmation.
+   *
+   * @get result
+   * @var string
+   */
+  public $dt_agree = '';
+
+  /**
+   * Text of online waiver.
+   *
+   * @get result
+   * @var string
+   */
+  public $html_contract = '';
+
+  /**
+   * The IP address from which the confirmation was carried out.
+   *
+   * @get result
+   * @var string
+   */
+  public $ip_agree = '';
+
+  /**
+   * User's name.
+   *
+   * @get result
+   * @var string
+   */
+  public $s_name = '';
+
+  /**
+   * URL to image with client's signature.
+   *
+   * @get result
+   * @var string
+   */
+  public $url_signature = '';
 }
 
 ?>
