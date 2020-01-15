336f21e374a6be365b127922ea9d1d45ff37e8cb
Gromakov <gromakov94@gmail.com>
2020-01-15 19:02:33 +0200

    wl-22846: Add the opportunity to change an avatar +review WL-21209

---
+++ WellnessLiving/Core/Drive/ImageUpload/ImageUploadModel.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace WellnessLiving\Core\Drive\ImageUpload;
+
+use WellnessLiving\WlModelAbstract;
+
+/**
+ * Performs final action action to image uploaded with
+ * {@link \WellnessLiving\Core\Drive\ImageUpload\ImageUploadTemporaryModel}.
+ */
+class ImageUploadModel extends WlModelAbstract
+{
+  /**
+   * Key of image within {@link ImageUploadModel::$s_class}.
+   *
+   * @put get
+   * @var string
+   */
+  public $k_id = '';
+
+  /**
+   * Name of class that manages this image.
+   *
+   * @put get
+   * @var string
+   */
+  public $s_class = '';
+
+  /**
+   * Action that must be done to image.
+   *
+   * @put post
+   * @var string
+   */
+  public $s_command = '';
+
+  /**
+   * Forms command to delete image.
+   */
+  public function commandDelete():void
+  {
+    $this->s_command = 'delete';
+  }
+
+  /**
+   * Forms command to save uploaded image.
+   */
+  public function commandSave():void
+  {
+    $this->s_command = 'save';
+  }
+
+  /**
+   * Forms command to crop and save uploaded image.
+   *
+   * @param int $i_offset_left Left offset to crop.
+   * @param int $i_offset_top Top offset to crop.
+   * @param int $i_width Width to crop.
+   * @param int $i_height Height to crop.
+   * @param int $i_rotation Rotation angle.
+   */
+  public function commandUpload(int $i_offset_left,int $i_offset_top,int $i_width,int $i_height,int $i_rotation = 0):void
+  {
+    $this->s_command = 'upload '.$i_offset_left.' '.$i_offset_top.' '.$i_width.' '.$i_height.' '.$i_rotation;
+  }
+}
+
+?>
