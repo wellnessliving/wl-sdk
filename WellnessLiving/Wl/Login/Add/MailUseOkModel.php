2bc74f714224caf92ff8af391c1cec40668a89a0
GreggHenry02 <gregghenry+github@gmail.com>
2020-02-26 14:11:41 -0500

    wl-23632: Create workflow for NOUS to add already existing clients to a Franchisee +review WL-23253 * Added endpoint missing from 5.5 SDK.

---
+++ WellnessLiving/Wl/Login/Add/MailUseOkModel.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace WellnessLiving\Wl\Login\Add;
+
+use WellnessLiving\WlModelAbstract;
+
+/**
+ * Registers existing user in current business.
+ * If user already member of current business then do nothing.
+ */
+class MailUseOkModel extends WlModelAbstract
+{
+  /**
+   * The list of fields missing information.
+   * Each element is an array containing the following data:<dl>
+   *   <dt>string <var>html_message</var></dt>
+   *   <dd>The error message.</dd>
+   *   <dt>string|null <var>s_field</var></dt>
+   *   <dd>The name of the field which is missing information.
+   *     Given in the format 'k_field.name'.
+   *   </dd>
+   * </dl>
+   *
+   * @post result
+   * @var array
+   */
+  public $a_error = [];
+
+  /**
+   * Whether this is a lead. <tt>true</tt> if this is the lead. <tt>false</tt> otherwise.
+   *
+   * @post post
+   * @var string
+   */
+  public $is_lead = 0;
+
+  /**
+   * Business key.
+   *
+   * @post post
+   * @var string
+   */
+  public $k_business = '';
+
+  /**
+   * Lead Widget skin key used to add the lead.
+   *
+   * @post post
+   * @var string
+   */
+  public $k_skin = '';
+
+  /**
+   * The result code of the request.
+   *
+   * @post result
+   * @var string
+   */
+  public $s_code = '';
+
+  /**
+   * The result message of the request.
+   *
+   * @post result
+   * @var string
+   */
+  public $s_message = '';
+
+  /**
+   * User ID.
+   *
+   * @post post
+   * @var string
+   */
+  public $uid = '';
+}
+
+?>
