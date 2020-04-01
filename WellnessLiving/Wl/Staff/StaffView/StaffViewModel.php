a55cb57f25f6c9e344baff814ea32e8a4cf826fe
anton-moskalenko <moskalenko.anton@gmail.com>
2020-04-01 18:59:17 +0300

    wl-24116: Extend documentation for StaffViewModel. +review WL-23853

--- WellnessLiving/Wl/Staff/StaffView/StaffViewModel.php
+++ WellnessLiving/Wl/Staff/StaffView/StaffViewModel.php
@@ -6,11 +6,28 @@ use WellnessLiving\WlModelAbstract;
 
 /**
  * Retrieves information about a staff member.
+ *
+ * This method can accept or one staff key {@link StaffViewApi::$k_staff} or staff list
+ * {@link StaffViewApi::$a_staff_list} but not both (exception would be thrown).
  */
 class StaffViewModel extends WlModelAbstract
 {
   /**
-   * Staff schedule.
+   * Contains staff schedule of classes per day.
+   *
+   * <dl>
+   *   <dt>array <var>a_class_period</var></dt>
+   *   <dd>List of sessions in day:<dl>
+   *     <dt>string <var>dt_time</var></dt>
+   *     <dd>Session start time in MySQL format.</dd>
+   *     <dt>int <var>i_duration</var></dt>
+   *     <dd>Session duration in seconds.</dd>
+   *     <dt>string <var>s_title</var></dt>
+   *     <dd>Class name.</dd>
+   *   </dl></dd>
+   *   <dt>int <var>i_day</var></dt>
+   *   <dd>Day of week. One of {@link ADateWeekSid} constants.</dd>
+   * </dl>
    *
    * <tt>null</tt> if data is not loaded yet.
    *
@@ -20,7 +37,45 @@ class StaffViewModel extends WlModelAbstract
   public $a_class_day = null;
 
   /**
-   * Result staff list.
+   * Staff list of staff data and class day.
+   *
+   * <dl>
+   *   <dt>array <var>a_class_day</var></dt>
+   *   <dd>
+   *     Contains schedule of classes per day:
+   *     <dl>
+   *       <dt>array <var>a_class_period</var></dt>
+   *       <dd>List of sessions in day:<dl>
+   *         <dt>string <var>dt_time</var></dt>
+   *         <dd>Session start time in MySQL format.</dd>
+   *         <dt>int <var>i_duration</var></dt>
+   *         <dd>Session duration in seconds.</dd>
+   *         <dt>string <var>s_title</var></dt>
+   *         <dd>Class name.</dd>
+   *       </dl></dd>
+   *       <dt>int <var>i_day</var></dt>
+   *       <dd>Day of week. One of {@link ADateWeekSid} constants.</dd>
+   *     </dl>
+   *   </dd>
+   *   <dt>array <var>a_staff</var></dt>
+   *   <dd>
+   *     Staff information:
+   *     <dl>
+   *       <dt>int <var>id_gender</var></dt>
+   *       <dd>Staff member's gender. One of {@link \AGenderSid} constants.</dd>
+   *       <dt>string <var>s_biography</var></dt>
+   *       <dd>Staff biography.</dd>
+   *       <dt>string <var>s_family</var></dt>
+   *       <dd>1st letter of surname.</dd>
+   *       <dt>string <var>s_name</var></dt>
+   *       <dd>Staff name.</dd>
+   *       <dt>string <var>s_position</var></dt>
+   *       <dd>Staff job name.</dd>
+   *       <dt>string <var>uid</var></dt>
+   *       <dd>Staff user ID.</dd>
+   *     </dl>
+   *   </dd>
+   * </dl>
    *
    * <tt>null</tt> if data is not loaded yet.
    *
@@ -30,7 +85,22 @@ class StaffViewModel extends WlModelAbstract
   public $a_result_list = null;
 
   /**
-   * Staff data.
+   * Staff information.
+   *
+   * <dl>
+   *   <dt>int <var>id_gender</var></dt>
+   *   <dd>Staff member's gender. One of {@link \AGenderSid} constants.</dd>
+   *   <dt>string <var>s_biography</var></dt>
+   *   <dd>Staff biography.</dd>
+   *   <dt>string <var>s_family</var></dt>
+   *   <dd>1st letter of surname.</dd>
+   *   <dt>string <var>s_name</var></dt>
+   *   <dd>Staff name.</dd>
+   *   <dt>string <var>s_position</var></dt>
+   *   <dd>Staff job name.</dd>
+   *   <dt>string <var>uid</var></dt>
+   *   <dd>Staff user ID.</dd>
+   * </dl>
    *
    * <tt>null</tt> if data is not loaded yet.
    *
