ec5f2e4a382ca4f488d2d59c49a77e221258905b
GreggHenry02 <gregghenry+github@gmail.com>
2020-11-13 09:32:04 -0500

    wl-27042: Allow GlobalFit to hold or cancel memberships as needed via API +review WL-27962 * Added PromotionPayPauseModel endpoint.

---
+++ WellnessLiving/Wl/Login/Promotion/PromotionPayPauseModel.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace WellnessLiving\Wl\Login\Promotion;
+
+use WellnessLiving\WlModelAbstract;
+
+/**
+ * Manages promotion payment pause periods.
+ */
+class PromotionPayPauseModel extends WlModelAbstract
+{
+  public const DATE_END_INDEFINITE = '0000-00-00';
+
+  /**
+   * End date of pause period in login promotion timezone. Can be set to special value
+   * {@link PromotionPayPauseApi::DATE_END_INDEFINITE} to make the period indefinite until further action.
+   *
+   * <tt>null</tt> if it shouldn't be updated.
+   *
+   * @get get,result
+   * @post get
+   * @put get
+   * @var string|null
+   */
+  public $dt_end = null;
+
+  /**
+   * Start date of pause period in login promotion timezone.
+   *
+   * <tt>null</tt> if it shouldn't be updated.
+   *
+   * @get get,result
+   * @post get
+   * @put get
+   * @var string|null
+   */
+  public $dt_start = null;
+
+  /**
+   * Key of business to which currently handled pause period or login promotion belongs.
+   * Primary key in {@link \RsBusinessSql} table.
+   *
+   * <tt>null</tt> if not initialized.
+   *
+   * @var string|null
+   */
+  private $k_business = null;
+
+  /**
+   * Key of login promotion to create pause for.
+   * Primary key in {@link \RsLoginPromotionSql} table.
+   *
+   * Ignored if {@link $k_promotion_pay_pause} is provided.
+   *
+   * <tt>null</tt> if not initialized.
+   *
+   * @delete get
+   * @get get,result
+   * @post get
+   * @var string|null
+   */
+  public $k_login_promotion = null;
+
+  /**
+   * Key of promotion to which login promotion or its pause period is linked.
+   * Primary key in {@link \RsPromotionSql} table.
+   *
+   * <tt>null</tt> if not initialized.
+   *
+   * @var string|null
+   */
+  private $k_promotion = null;
+
+  /**
+   * Key of the payment pause period to read or update.
+   * Primary key in {@link \RsPromotionPayPauseSql} table.
+   *
+   * <tt>null</tt> if not initialized or request is based on {@link $k_login_promotion}.
+   *
+   * @delete get
+   * @get get,result
+   * @post get,result
+   * @put get,result
+   * @var string|null
+   */
+  public $k_promotion_pay_pause = null;
+
+  /**
+   * Additional notes for promotion payment pause period.
+   *
+   * <tt>null</tt> if it shouldn't be updated.
+   *
+   * @get result
+   * @post post
+   * @put put
+   * @var string|null
+   */
+  public $text_note = null;
+
+  /**
+   * Key of the client to which login promotion or its pause period belongs.
+   * Primary key in {@link \PassportLoginSql} table.
+   *
+   * <tt>null</tt> if not initialized.
+   *
+   * @var string|null
+   */
+  private $uid = null;
+}
+
+?>
