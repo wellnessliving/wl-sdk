8540269d9991afceafccb31e60e7a18735b2a324
ilya.polyakov <ilya@wellnessliving.com>
2021-02-15 16:42:05 +0200

    wl-28455: JS error has occurred after attempt to book recurring +review WL

--- WellnessLiving/Wl/Appointment/Book/Finish/FinishModel.php
+++ WellnessLiving/Wl/Appointment/Book/Finish/FinishModel.php
@@ -8,6 +8,8 @@ use WellnessLiving\Wl\Appointment\WlAppointmentPaySid;
 
 /**
  * Model to complete booking appointment.
+ * 
+ * @deprecated Use {@link FinishModel_47} instead.
  */
 class FinishModel extends WlModelAbstract
 {
