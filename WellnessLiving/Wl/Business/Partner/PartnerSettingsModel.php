cecbe02d06728c60ab09b998e76d6bdce978e35b
Alex <tutnoswbox@gmail.com>
2021-02-16 13:17:04 +0300

    wl-28632: To expand the capabilities of the API: +review WL-29516

---
+++ WellnessLiving/Wl/Business/Partner/PartnerSettingsModel.php
@@ -0,0 +1,79 @@
+<?php 
+
+namespace WellnessLiving\Wl\Business\Partner;
+
+use WellnessLiving\WlModelAbstract;
+
+/**
+ * Works with partner settings of the business: name of the business owner, review about wellnessliving and other.
+ * This information staff member enters, when enrolls into partner program and is used on custom landing page, which
+ * is available by special business url.
+ */
+class PartnerSettingsModel extends WlModelAbstract
+{
+  /**
+   * Business key.
+   *
+   * @get result
+   * @var string
+   */
+  public $k_business = '';
+
+  /**
+   * Home page tour of the business. Depends on type.
+   *
+   * @get result
+   * @var string
+   */
+  public $text_business_tour = '';
+
+  /**
+   * Unique code that business can give other businesses to tell them about system.
+   *
+   * @get get
+   * @var string
+   */
+  public $text_code = '';
+
+  /**
+   * First name of the business representative.
+   *
+   * @get result
+   * @var string
+   */
+  public $text_name_first;
+
+  /**
+   * Last name of the business representative.
+   *
+   * @get result
+   * @var string
+   */
+  public $text_name_last;
+
+  /**
+   * Job title of the business representative.
+   *
+   * @get result
+   * @var string
+   */
+  public $text_position;
+
+  /**
+   * Review from the business representative.
+   *
+   * @get result
+   * @var string
+   */
+  public $text_review;
+
+  /**
+   * Link to the photo of the business representative.
+   *
+   * @get result
+   * @var string
+   */
+  public $url_photo;
+}
+
+?>
