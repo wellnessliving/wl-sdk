c0b86396eef08f3c20914c49d33ac68cd43503da
sergey.sydorenko <sergey.sydorenko@wellnessliving.com>
2021-05-03 20:10:34 +0300

    wl-29615: Add applying of new notification settings to email sending +review WL

--- WellnessLiving/Wl/Business/User/Subscribe/SubscribeModel.php
+++ WellnessLiving/Wl/Business/User/Subscribe/SubscribeModel.php
@@ -24,6 +24,20 @@ class SubscribeModel extends WlModelAbstract
   public $is_subscribe = null;
 
   /**
+   * Information about user`s subscription.
+   * In case of getting information,
+   * <tt>true</tt> if user has sms subscription on business, <tt>false</tt> otherwise.
+   * In case of changing subscription,
+   * <tt>true</tt> if subscribe user on business, <tt>false</tt> is unsubscribe user on business.
+   * <tt>null</tt> until loaded.
+   *
+   * @get result
+   * @put get
+   * @var bool|null
+   */
+  public $is_subscribe_sms = null;
+
+  /**
    * Business key.
    * Key of the business for which user will subscribe, unsubscribe,
    * receive information about the status of the subscription.
