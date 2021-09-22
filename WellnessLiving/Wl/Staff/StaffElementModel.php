0fd422f7bf962b71521dfbc7732b78098ff42419
AndreyZubkov96 <zubkov.andrey96@gmail.com>
2021-09-22 14:59:58 +0300

    wl-31765: Add api to create staff member * change name with prefix s_ on text_ in StaffElementModel +review WL-33336

--- WellnessLiving/Wl/Staff/StaffElementModel.php
+++ WellnessLiving/Wl/Staff/StaffElementModel.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 namespace WellnessLiving\Wl\Staff;
 
@@ -9,95 +9,95 @@ use WellnessLiving\WlModelAbstract;
  */
 class StaffElementModel extends WlModelAbstract
 {
-  /**
-   * Should staff member be shown on the directory site of the business.
-   *
-   * `null` means to not change the current value of the field.
-   *
-   * @post post
-   * @var bool|null
-   */
-  public $is_microsite;
+    /**
+     * Should staff member be shown on the directory site of the business.
+     *
+     * `null` means to not change the current value of the field.
+     *
+     * @post post
+     * @var bool|null
+     */
+    public $is_microsite;
 
-  /**
-   * Key of the business to get\change staff member in.
-   *
-   * Primary key in the {@link \RsBusinessSql}.
-   *
-   * <tt>null</tt> if video does not have thumbnail image.
-   *
-   * @post get
-   * @var string
-   */
-  public $k_business;
+    /**
+     * Key of the business to get\change staff member in.
+     *
+     * Primary key in the {@link \RsBusinessSql}.
+     *
+     * <tt>null</tt> if video does not have thumbnail image.
+     *
+     * @post get
+     * @var string
+     */
+    public $k_business;
 
-  /**
-   * Key of the staff member to be changed.
-   *
-   * Primary key in the {@link \RsStaffSql}.
-   *
-   * <tt>null</tt> if new staff member should be created.
-   *
-   * @post get,result
-   * @var string|null
-   */
-  public $k_staff;
+    /**
+     * Key of the staff member to be changed.
+     *
+     * Primary key in the {@link \RsStaffSql}.
+     *
+     * <tt>null</tt> if new staff member should be created.
+     *
+     * @post get,result
+     * @var string|null
+     */
+    public $k_staff;
 
-  /**
-   * Staff email.
-   *
-   * @post post
-   * @var string|null
-   */
-  public $s_email;
+    /**
+     * Staff email.
+     *
+     * @post post
+     * @var string|null
+     */
+    public $text_email;
 
-  /**
-   * Staff first name.
-   *
-   * @post post
-   * @var string|null
-   */
-  public $s_first_name;
+    /**
+     * Staff first name.
+     *
+     * @post post
+     * @var string|null
+     */
+    public $text_first_name;
 
-  /**
-   * Staff last name.
-   *
-   * @post post
-   * @var string|null
-   */
-  public $s_last_name;
+    /**
+     * Staff last name.
+     *
+     * @post post
+     * @var string|null
+     */
+    public $text_last_name;
 
-  /**
-   * Password.
-   *
-   * @post post
-   * @var string|null
-   */
-  public $s_password;
+    /**
+     * Password.
+     *
+     * @post post
+     * @var string|null
+     */
+    public $text_password;
 
-  /**
-   * Confirmation of password.
-   *
-   * @post post
-   * @var string|null
-   */
-  public $s_password_confirm;
+    /**
+     * Confirmation of password.
+     *
+     * @post post
+     * @var string|null
+     */
+    public $text_password_confirm;
 
-  /**
-   * Staff job title.
-   *
-   * @post post
-   * @var string|null
-   */
-  public $s_position;
+    /**
+     * Staff job title.
+     *
+     * @post post
+     * @var string|null
+     */
+    public $text_position;
 
-  /**
-   * User key. Primary key in {@link \PassportLoginSql} table.
-   *
-   * @get get
-   * @var string
-   */
-  public $uid;
+    /**
+     * User key. Primary key in {@link \PassportLoginSql} table.
+     *
+     * @get get
+     * @var string
+     */
+    public $uid;
 }
 
 ?>
