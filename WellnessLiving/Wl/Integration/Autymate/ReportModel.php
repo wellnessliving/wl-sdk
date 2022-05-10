e2adcde0985ed0ce3b81318c8b1cfa2ba78c09ba
GreggHenry02 <gregghenry+github@gmail.com>
2022-05-10 14:53:53 -0400

    wl-36066: Create an API endpoint to access Sales Transaction Beta report +review WL-37515 * Added new endpoint to simplify access to All Transactions report data. * Added new SID values for report generator status.

---
+++ WellnessLiving/Wl/Integration/Autymate/ReportModel.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace WellnessLiving\Wl\Integration\Autymate;
+
+use WellnessLiving\WlModelAbstract;
+
+/**
+ * Retrieves all daily transactions for a business using the All Transactions report and the DataApi endpoint.
+ * This endpoint is a streamlined interface for easier access.
+ */
+class ReportModel extends WlModelAbstract
+{
+  /**
+   * A list of fields in this report.
+   *
+   * This array is effectively a title row for table that is returned in {@link \WellnessLiving\Wl\Integration\Autymate\ReportModel::$a_row}.
+   *
+   * @get result
+   * @var string[]
+   */
+  public $a_field;
+
+  /**
+   * Report data.
+   *
+   * This is an indexed array in which one row is an indexed array also.
+   *
+   * Indexes of the columns correspond columns in {@link \WellnessLiving\Wl\Integration\Autymate\ReportModel::$a_field}.
+   *
+   * @get result
+   * @var string[][]
+   */
+  public $a_row;
+
+  /**
+   * Warning list of the report.
+   *
+   * @get result
+   * @var string[]
+   */
+  public $a_warning;
+
+  /**
+   * Date in local time to retrieve transactions for.
+   *
+   * @get get
+   * @var string
+   */
+  public $dtl_date = '';
+
+  /**
+   * The page of the report, starting from 0.
+   *
+   * @get get
+   * @var int
+   */
+  public $i_page=0;
+
+  /**
+   * Status of the report.
+   *
+   * One of {@link \Wl\Report\Generator\ReportGeneratorStatusSid} constants.
+   *
+   * @get result
+   * @var int
+   */
+  public $id_report_status;
+
+  /**
+   * If <tt>true</tt> then there are more report rows to get. Otherwise <tt>false</tt>, all rows are sent.
+   *
+   * @get result
+   * @var bool
+   */
+  public $is_more;
+
+  /**
+   * ID of business for which report must be generated.
+   *
+   * @get get
+   * @var string
+   */
+  public $k_business = '0';
+
+  /**
+   * User's ID.
+   *
+   * Primary key in {@link \PassportLoginSql}.
+   *
+   * @get get
+   * @var string
+   */
+  public $uid = '0';
+}
+?>
