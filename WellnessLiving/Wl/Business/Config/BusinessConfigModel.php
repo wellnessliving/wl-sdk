fcce3a5cd8c3fa88661140cfce5f9daef169b301
DariusHatami <hatami.darius@gmail.com>
2022-08-24 16:06:06 -0400

    Tech Comm revision of SDK documentation * Check documentation for BusinessConfigModel.php, BusinessFranchiseLocationModel.php, BusinessFranchiseLocationSid.php

--- WellnessLiving/Wl/Business/Config/BusinessConfigModel.php
+++ WellnessLiving/Wl/Business/Config/BusinessConfigModel.php
@@ -6,7 +6,7 @@ use WellnessLiving\WlModelAbstract;
 use WellnessLiving\WlServiceSid;
 
 /**
- * Manage business configurations related to clients, bookings, payments, etc.
+ * Manages business configurations for clients, bookings, payments, and related things.
  */
 class BusinessConfigModel extends WlModelAbstract
 {
@@ -15,17 +15,17 @@ class BusinessConfigModel extends WlModelAbstract
    *
    * <dl>
    *   <dt>int <var>a_wait_service</var></dt>
-   *   <dd>Keys are list of IDs from {@link WlServiceSid}, and values are flags whether wait list is allowed.</dd>
+   *   <dd>The keys are listed as IDs from {@link WlServiceSid}, and values are flags outlining whether wait listing is allowed.</dd>
    *   <dt>int <var>i_book_before</var></dt>
-   *   <dd> Minimum hours|days|months before class should be to book.</dd>
+   *   <dd>The minimum hours|days|months before a class can be booked.</dd>
    *   <dt>int <var>i_book_future</var></dt>
-   *   <dd> Maximum hours|days|months after class can be to book.</dd>
+   *   <dd>The maximum hours|days|months after a class can be booked.</dd>
    *   <dt>int <var>i_cancel</var></dt>
-   *   <dd> Minimum hours|days|months before class should be to cancel without penalty.</dd>
+   *   <dd>The minimum hours|days|months before a class can be canceled without penalty.</dd>
    *   <dt>int <var>i_promote</var></dt>
-   *   <dd> Minimum hours|days|months before class should be to promote from wait list.</dd>
+   *   <dd>The minimum hours|days|months before a class can be promoted from a wait list.</dd>
    *   <dt>int <var>i_reattempt_count</var></dt>
-   *   <dd>Number of failed auto-payments reattempts.</dd>
+   *   <dd>The number of failed auto-payment reattempts.</dd>
    *   <dt>int <var>id_book_before</var></dt>
    *   <dd>Hours|days|months from {@link ADurationSid}.</dd>
    *   <dt>int <var>id_book_future</var></dt>
@@ -35,29 +35,29 @@ class BusinessConfigModel extends WlModelAbstract
    *   <dt>int <var>id_promote</var></dt>
    *   <dd>Hours|days|months from {@link ADurationSid}.</dd>
    *   <dt>bool <var>is_book_inside_active_pay_period</var></dt>
-   *   <dd>if `true` - clients with purchase options are only allowed
-   *     to book sessions within their current paid period, `false` - during purchase option's duration.</dd>
+   *   <dd>if `true` - clients with Purchase Options are only allowed
+   *     to book sessions within their current paid period, `false` - during Purchase Option's duration.</dd>
    *   <dt>int <var>is_disable_promotion</var></dt>
    *   <dd>1 if a client's automatic payment fails, their account should not be
-   *     debited and their purchase option becomes inactive, 0 - otherwise. Default 0.</dd>
+   *     debited and their Purchase Option becomes inactive, 0 - otherwise. Default 0.</dd>
    *   <dt>bool <var>is_enable_payment_penalty</var></dt>
-   *   <dd>Whether to charge penalty after final auto-payment attempt.</dd>
+   *   <dd>Determines whether to charge a penalty after the final auto-payment attempt.</dd>
    *   <dt>bool <var>is_enable_payment_reattempt</var></dt>
-   *   <dd>Whether to reattempt failed auto-payments.</dd>
+   *   <dd>Determines whether to reattempt failed auto-payments.</dd>
    *   <dt>bool <var>is_enable_staff_ip_restriction</var></dt>
-   *   <dd>Whether to restrict which IP addresses staff can login from.</dd>
+   *   <dd>Determines whether to restrict which IP addresses staff can login from.</dd>
    *   <dt>int <var>is_prevent_booking</var></dt>
    *   <dd>1 if booking for a client with negative balance is disabled, 0 - otherwise. Default 0.</dd>
    *   <dt>bool <var>is_staff_restrict</var></dt>
-   *   <dd>If true, client can not choose provider while appointment wizard.</dd>
+   *   <dd>If true, clients can't choose a provider in the appointment wizard.</dd>
    *   <dt>bool <var>is_wait</var></dt>
    *   <dd>Enable\disable wait list.</dd>
    *   <dt>int <var>k_currency</var></dt>
-   *   <dd>Currency from rs.currency table.</dd>
+   *   <dd>The currency from rs.currency table.</dd>
    *   <dt>int <var>k_timezone</var></dt>
-   *   <dd>Timezone from get.timezone table.</dd>
+   *   <dd>The timezone from get.timezone table.</dd>
    *   <dt>string <var>m_payment_penalty</var></dt>
-   *   <dd>Penalty amount to charge after final auto-payment attempt.</dd>
+   *   <dd>The penalty amount to charge after the final auto-payment attempt.</dd>
    * </dl>
    *
    * @get result
@@ -66,9 +66,9 @@ class BusinessConfigModel extends WlModelAbstract
   public $a_business_policy;
 
   /**
-   * Whether staff should select a location at checkout.
+   * Determines whether staff members should select a location at checkout.
    *
-   * `true` - staff should select a location at checkout, `false` - otherwise.
+   * `true` - staff members should select a location at checkout, `false` - otherwise.
    *
    * @get result
    * @var bool
@@ -76,7 +76,7 @@ class BusinessConfigModel extends WlModelAbstract
   public $is_location_select;
 
   /**
-   * Business key. Primary key in {@link \RsBusinessSql} table.
+   * The business key. The primary key in {@link \RsBusinessSql} table.
    *
    * @get get
    * @var string
