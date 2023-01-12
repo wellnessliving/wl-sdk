d1197422940fd98218b743d8c6345b4674307aff
DariusHatami <hatami.darius@gmail.com>
2023-01-12 15:36:11 -0500

    Tech Comm revision of SDK documentation * Check documentation for CurvesFranchiseLocationModel.php, SamlUserDeactivationModel.php, LeadModel.php, FlagModel.php

--- WellnessLiving/Wl/Lead/LeadModel.php
+++ WellnessLiving/Wl/Lead/LeadModel.php
@@ -5,11 +5,11 @@ namespace WellnessLiving\Wl\Lead;
 use WellnessLiving\WlModelAbstract;
 
 /**
- * Gets information from the "Lead capture" widget and saves the new user’s information.
+ * An endpoint that gets information from the Lead Capture widget and saves a new user’s information.
  *
- * A user can be added to a second business by adding them first as a lead. If your business uses Franchise Cloud,
- * there may be a restriction where a client can only be a member in one Franchisee, they will be a traveller in all
- * other Franchisees.
+ * A user can be added to a second business by adding them first as a lead. If your business uses Enterprise Cloud,
+ * there may be a restriction where clients can only be members in one enterprise location (travellers in all
+ * other enterprise locations).
  */
 class LeadModel extends WlModelAbstract
 {
@@ -32,8 +32,8 @@ class LeadModel extends WlModelAbstract
    *   <dd>
    *     A list of possible options for an HTML select field. This value is only used if this field is an HTML select.
    *     Every element has the following keys:
-   *     <dl><dt>string <var>s_id</var></dt><dd>Option ID.</dd>
-   *     <dt>string <var>text_title</var></dt><dd>Option title.</dd></dl>
+   *     <dl><dt>string <var>s_id</var></dt><dd>The option ID.</dd>
+   *     <dt>string <var>text_title</var></dt><dd>The option title.</dd></dl>
    *   </dd>
    *   <dt>
    *     int <var>id_field_general</var>
@@ -52,7 +52,7 @@ class LeadModel extends WlModelAbstract
    *     bool <var>is_require</var>
    *   </dt>
    *   <dd>
-   *     If `true`, then the field is mandatory. If `false`, then the field is not mandatory.
+   *     If `true`, then the field is mandatory. If `false`, then the field isn't mandatory.
    *   </dd>
    *   <dt>
    *     string <var>k_field</var>
@@ -101,9 +101,9 @@ class LeadModel extends WlModelAbstract
   public $k_business = '0';
 
   /**
-   * The ID of the widget skin. If left empty then the default skin is used.
+   * The ID of the widget skin. If left empty, then the default skin is used.
    * This will be the Lead Capture widget skin with ‘Use this widget for the Add Lead form’ selected.
-   * If your business doesn’t have a skin selected this endpoint will default to the system wide default,
+   * If your business doesn't have a skin selected, this endpoint will default to the system-wide default,
    * which may lack fields your business requires when adding a lead or a client.
    *
    * @get get
@@ -114,8 +114,8 @@ class LeadModel extends WlModelAbstract
 
   /**
    * The characters entered by the lead for the captcha test.
-   * This is not necessary if the GET method returned an empty url_captcha.
-   * This field is not necessary if the GET method returned an empty
+   * This isn't necessary if the GET method returned an empty url_captcha.
+   * This field isn't necessary if the GET method returned an empty
    * {@link \WellnessLiving\Wl\Lead\LeadModel::$url_captcha}.
    *
    * @post post
@@ -133,7 +133,7 @@ class LeadModel extends WlModelAbstract
 
   /**
    * The URL to load the image with a captcha test.
-   * This string is empty if it is not necessary to pass a captcha test.
+   * This string is empty if it's not necessary to pass a captcha test.
    *
    * @get result
    * @var string
