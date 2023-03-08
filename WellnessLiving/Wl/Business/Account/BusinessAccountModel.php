e33a65efad8cea3d0463a88d470b7b96150791a8
alexandr.titov <titov@wellnessliving.com>
2023-03-08 16:03:53 +0300

    wl-44427: [WL Explorer] Business/Account/BusinessAccountModel.php - add prospect businesses Added ability to get prospect businesses from the model, which returned only customers before.

--- WellnessLiving/Wl/Business/Account/BusinessAccountModel.php
+++ WellnessLiving/Wl/Business/Account/BusinessAccountModel.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 namespace WellnessLiving\Wl\Business\Account;
 
@@ -6,6 +6,9 @@ use WellnessLiving\WlModelAbstract;
 
 /**
  * An API used to get a list of active businesses.
+ *
+ * Is not available without user authorization.
+ * Requires authorized user to have privilege `rs.business.view`.
  */
 class BusinessAccountModel extends WlModelAbstract
 {
@@ -16,6 +19,16 @@ class BusinessAccountModel extends WlModelAbstract
    * @var string[]
    */
   public $a_business_keys = [];
+
+  /**
+   * `true` to return only wellnesliving paid customers.
+   * `false` to return prospects.
+   *
+   * Test and churned businesses are never returned.
+   *
+   * @var bool
+   */
+  public $is_prospects = false;
 
   /**
    * Specifies if only businesses having published locations should be returned.
