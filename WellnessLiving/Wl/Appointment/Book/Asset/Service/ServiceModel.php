57c09e0623486b16a0f6cfa6d3b887eacd504199
DariusHatami <hatami.darius@gmail.com>
2023-04-14 15:17:52 -0400

    Tech Comm revision of SDK documentation * Check documentation for ServiceModel.php, AssetListModel.php

--- WellnessLiving/Wl/Appointment/Book/Asset/Service/ServiceModel.php
+++ WellnessLiving/Wl/Appointment/Book/Asset/Service/ServiceModel.php
@@ -5,22 +5,22 @@ namespace WellnessLiving\Wl\Appointment\Book\Asset\Service;
 use WellnessLiving\WlModelAbstract;
 
 /**
- * An endpoint for assets which are required for a service booking.
+ * An endpoint that retrieves assets that are required for a service booking.
  */
 class ServiceModel extends WlModelAbstract
 {
   /**
    * A list of reserved assets.
    *
-   * 1st level keys - asset keys.
-   * 2nd level keys - asset numbers.
-   * Values - keys of appointment bookings that reserves the asset(s).
+   * 1st level keys refer to asset keys.
+   * 2nd level keys refer to asset numbers.
+   * Values refer to keys of appointment bookings that reserve the asset(s).
    *
    * For example, if you want to check if the 10th asset with the key of '15' is reserved,
-   * you can check if <tt>a_resource_busy['15']['10']</tt> is not empty.
+   * you can check if `a_resource_busy['15']['10']` is free.
    *
-   * If you're rebooking an appointment, check the value of <tt>a_resource_busy['15']['10']</tt>.
-   * If it's equal to the key of your current appointment booking, then consider the asset as available.
+   * If you're rebooking an appointment, check the value of `a_resource_busy['15']['10']`.
+   * If it's equal to the key of your current appointment booking, you can assume the asset is available.
    *
    * @get result
    * @var array
@@ -28,15 +28,15 @@ class ServiceModel extends WlModelAbstract
   public $a_resource_busy = [];
 
   /**
-   * A list of assets which are required for the service booking.
-   * Keys - the asset category key(s).
-   * Values - sub arrays with next key(s).
+   * A list of assets required for the service booking.
+   * Keys refer the asset category key(s).
+   * Values refer to sub-arrays with next key(s).
    * <dl>
    *   <dt>
    *     array[] <var>a_resource</var>
    *   </dt>
    *   <dd>
-   *     A list of resources. Every element has the keys:
+   *     A list of resources. Every element has the following keys:
    *     <dl>
    *       <dt>
    *         array <var>a_image</var>
@@ -48,8 +48,8 @@ class ServiceModel extends WlModelAbstract
    *         bool <var>is_available</var>
    *       </dt>
    *       <dd>
-   *         If <tt>true</tt>, then this asset has at least one free unit.
-   *         If <tt>false</tt>, then this asset has no free units.
+   *         If `true`, then this asset has at least one free unit.
+   *         If `false`, then this asset has no free units.
    *       </dd>
    *       <dt>
    *         string <var>k_resource</var>
@@ -69,7 +69,7 @@ class ServiceModel extends WlModelAbstract
    *     string|null <var>k_resource_layout</var>
    *   </dt>
    *   <dd>
-   *     The asset's layout key. <tt>null</tt> if the asset category has no layout.
+   *     The asset's layout key. This will be `null` if the asset category has no layout.
    *   </dd>
    *   <dt>
    *     string <var>s_resource_type</var>
@@ -87,7 +87,7 @@ class ServiceModel extends WlModelAbstract
   /**
    * Can the staff members book reserved assets.
    *
-   * <tt>null</tt> if not loaded yet.
+   * This will be `null` if not loaded yet.
    *
    * @get result
    * @var bool
@@ -103,11 +103,11 @@ class ServiceModel extends WlModelAbstract
   public $dt_start = '';
 
   /**
-   * <tt>true</tt> for a case of backend mode.
+   * If `true`, back-end mode is selected.
    * In this mode, the list of asset categories must be returned even if asset selection is disabled for clients.
    *
-   * <tt>false</tt> for a case of frontend mode.
-   * In this mode, the list of asset categories must not be returned if asset selection is disabled for clients.
+   * If `false` for a case of front-end mode.
+   * In this mode, the list of asset categories won't be returned if asset selection is disabled for clients.
    *
    * @get get
    * @var bool
