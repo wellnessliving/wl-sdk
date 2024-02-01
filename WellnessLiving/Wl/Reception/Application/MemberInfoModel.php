32b2a0f95d1429f038090882de80953ea5e6c28c
NicholasPeraverson <Nicholas.P@Wellnessliving.com>
2024-02-01 15:31:03 -0500

    wl-56290: [RN Client SignIn] - Add type and message fields for a_items in MemberInfoApi +review WL-51193

--- WellnessLiving/Wl/Reception/Application/MemberInfoModel.php
+++ WellnessLiving/Wl/Reception/Application/MemberInfoModel.php
@@ -10,6 +10,14 @@ use WellnessLiving\WlModelAbstract;
 class MemberInfoModel extends WlModelAbstract
 {
   /**
+   * Additional member data or `null` if any data can be shown. Will be filled if {@link InfoApi::$is_full} flag is set.
+   *
+   * @get result
+   * @var array|null
+   */
+  public $a_info = null;
+
+  /**
    * Items, which are prepared for application.
    *
    * They are generated here, because js has limitation for work with translated messages.
@@ -39,6 +47,70 @@ class MemberInfoModel extends WlModelAbstract
   public $a_items = [];
 
   /**
+   * List of users data.
+   * Keys - users primary keys. Values - users data.
+   *
+   * `null` if data of a single user is requested.
+   *
+   * @get result
+   * @var array|null
+   */
+  public $a_result_list = null;
+
+  /**
+   * Primary keys of users whose information must be returned.
+   *
+   * `null` if data of a single user is requested.
+   *
+   * @get get
+   * @var string[]|null
+   */
+  public $a_uid = null;
+
+  /**
+   * Date of the session, if we show it on the appointment info window or on the attendance list.
+   *
+   * @get get
+   * @var string
+   */
+  public $dt_date = '';
+
+  /**
+   * `true` - if API is being used from backend, `false` - otherwise.
+   *
+   * @get get
+   * @var bool
+   */
+  public $is_backend = false;
+
+  /**
+   * If you need to return additional information set to `true` or `false` if not.
+   *
+   * @get get
+   * @var bool
+   */
+  public $is_full = false;
+
+  /**
+   * `true` if user is traveller; `false` otherwise.
+   * `null` in case when user is walk-in or when {@link MemberInfoApi::$is_full} is `false`.
+   *
+   * @get result
+   * @var bool|null
+   */
+  public $is_traveller = null;
+
+  /**
+   * ID of a business to retrieve information within.
+   *
+   * 0 to retrieve system-wide version of information.
+   *
+   * @get get
+   * @var string
+   */
+  public $k_business = '0';
+
+  /**
    * Key of the location, where Self Check In application is run.
    *
    * @get get
@@ -47,12 +119,77 @@ class MemberInfoModel extends WlModelAbstract
   public $k_location = '';
 
   /**
+   * ID of the visit, if we show icons on the attendance list and information that depends on visit is required.
+   *
+   * @get get
+   * @var string
+   */
+  public $k_visit = '';
+
+  /**
+   * Member ID. `null` if specified user is not a member of specified business.
+   *
+   * @get result
+   * @var string|null
+   */
+  public $s_member = null;
+
+  /**
    * Key of the Check In application.
    *
    * @get get
    * @var string
    */
   public $s_secret = '';
+
+  /**
+   * List of icons with additional information about business member.
+   * If empty, then all available icons will be displayed.
+   *
+   * @get get
+   * @var string
+   */
+  public $s_show = '';
+
+  /**
+   * First user name.
+   *
+   * @get result
+   * @var string
+   */
+  public $text_first_name = '';
+
+  /**
+   * Full user name.
+   *
+   * @get result
+   * @var string
+   */
+  public $text_fullname = '';
+
+  /**
+   * ID of a user to retrieve member information for.
+   *
+   * @get get
+   * @var string
+   */
+  public $uid = '';
+
+  /**
+   * URL to barcode image.
+   *
+   * @get result
+   * @var string
+   */
+  public $url_barcode;
+
+  /**
+   * URL to email.
+   *
+   * @get result
+   * @var string
+   */
+  public $url_email = '';
 }
 
 ?>
