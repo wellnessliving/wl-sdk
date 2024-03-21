b8927ea582261da82e9c704aaacd61089e9d1a6a
Dmytro Yermola <dmitriy.ermola@gmail.com>
2024-03-21 16:19:59 +0100

    Remove spaces

--- WellnessLiving/WlPayOwnerSid.php
+++ WellnessLiving/WlPayOwnerSid.php
@@ -4,7 +4,7 @@ namespace WellnessLiving;
 
 /**
  * @deprecated from 2023-09-11.
- * 
+ *
  * Use class {@link \WellnessLiving\Wl\WlPayOwnerSid} instead.
  */
 class WlPayOwnerSid extends \WellnessLiving\Wl\WlPayOwnerSid
