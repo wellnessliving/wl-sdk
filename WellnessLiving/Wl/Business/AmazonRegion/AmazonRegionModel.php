2912b4e3015c970a2cd81129f4193700afd0e67e
Vladislav Kobzev <kp42@ya.ru>
2024-03-22 12:48:05 +0300

    wl-55454: Add Wl_Business_BusinessAccessModel  to the PHP SDK +review WL-52266

---
+++ WellnessLiving/Wl/Business/AmazonRegion/AmazonRegionModel.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Wl\Business\AmazonRegion;
+
+use WellnessLiving\Wl\WlRegionSid;
+use WellnessLiving\WlModelAbstract;
+
+/**
+ * Endpoint for retrieve region ID for businesses.
+ */
+class AmazonRegionModel extends WlModelAbstract
+{
+  /**
+   * List of business key.
+   *
+   * @get get
+   * @var string[]
+   */
+  public $a_business = [];
+
+  /**
+   * List of region IDs for {@link AmazonRegionModel::$a_business}.
+   *
+   * Key - is business key.
+   * Value - is region ID. One of {@link WlRegionSid} constants.
+   *   `0` if this business is form an unknown region.
+   *
+   * @get result
+   * @var int[]
+   */
+  public $a_business_region;
+}
+
+?>
