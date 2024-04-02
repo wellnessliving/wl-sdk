9720ad1778c2b894ca5a28b07a68f9ab95de4f35
Alex Titov <titov@wellnessliving.com>
2024-04-02 17:47:25 +0300

    Add model to send notifications to users.

---
+++ WellnessLiving/Wl/Notification/Send/NotificationSendModel.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace WellnessLiving\Wl\Notification\Send;
+
+use WellnessLiving\WlModelAbstract;
+
+/**
+ * An endpoint that allows to send notifications to users of the certain business or location.
+ *
+ * This endpoint is available only for user with a special privilege. If you want to use this endpoint, contant
+ * with WellnessLiving support first.
+ */
+class NotificationSendModel extends WlModelAbstract
+{
+  /**
+   * Custom array with information which can be used to generate notification.
+   *
+   * @post post
+   * @var array
+   */
+  public $a_data = [];
+
+  /**
+   * Date and time in UTC, when notification should be sent. Empty string means to send immediately.
+   *
+   * If date and time are set, it should be reasonable. If it's too far in the future, API returns an error.
+   *
+   * @post post
+   * @var string
+   */
+  public $dtu_send = '';
+
+  /**
+   * ID of the notification.
+   *
+   * Contact with WellnessLiving support to get the list of available notifications.
+   *
+   * @post post
+   * @var int
+   */
+  public $id_notification = 0;
+
+  /**
+   * Key of the business where notification should be sent.
+   *
+   * @post post
+   * @var string
+   * @see \RsBusinessSql
+   */
+  public $k_business = '0';
+
+  /**
+   * Key of the location.
+   * If it's not empty, only clients from this location will get notification.
+   * `null` to not limit recipients with a certain location.
+   *
+   * @post post
+   * @var string|null
+   * @see \RsLocationSql
+   */
+  public $k_location = null;
+
+  /**
+   * List of UIDs joined with comma, if notification should be sent to certain recipients.
+   * Empty string means to send to all clients of the business or location with proper subscription level.
+   *
+   * @post post
+   * @var string
+   */
+  public $s_uid = '';
+}
+
+?>
