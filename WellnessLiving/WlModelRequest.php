879ac3a50ea61c677b4c22ea5a4891bd5adfee1a
yuri.popoff <yuri@wellnessliving.com>
2024-04-17 11:48:06 -0400

    wl-59108: [sdk] Fix bugs with global cookies +review WL-52816

--- WellnessLiving/WlModelRequest.php
+++ WellnessLiving/WlModelRequest.php
@@ -12,7 +12,7 @@ class WlModelRequest
   /**
    * SDK version number.
    */
-  const VERSION='202402241120';
+  const VERSION='202404171145';
 
   /**
    * A list of headers for the API request. See {@link CURLOPT_HTTPHEADER}.
