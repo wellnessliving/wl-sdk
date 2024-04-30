71e9b8733e5c6477e45bb20067cf16698eb4bb4d
elenakononskaya <lilylena24@gmail.com>
2024-04-30 19:15:23 +0300

    wl-59420: [API] Create an endpoint checking if user belongs to certain integrations +review WL-53077

---
+++ WellnessLiving/Wl/User/Info/UserIntegrationModel.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace WellnessLiving\Wl\User\Info;
+
+use WellnessLiving\WlModelAbstract;
+
+/**
+ * An endpoint that return the information about the integrations the user is connected to.
+ */
+class UserIntegrationModel extends WlModelAbstract
+{
+  /**
+   * Information about the integrations the user is connected to. The information returned has the following structure:
+   * <dl>
+   *  <dt>bool <var>is_classpass</var></dt>
+   *  <dd>`true` if user use the Classpass integration, `false` - otherwise.</dd>
+   *  <dt>bool <var>is_gympass</var></dt>
+   *  <dd>`true` if user use the Gympass integration, `false` - otherwise.</dd>
+   *  <dt>bool <var>is_reserve_with_google</var></dt>
+   *  <dd>`true` if user use the Reserve With Google integration, `false` - otherwise.</dd>
+   * </dl>
+   * `null` if the user does not belong to any integration.
+   *
+   * @get result
+   * @var array|null
+   */
+  public $a_integration;
+
+  /**
+   * The key of the business.
+   *
+   * @get get
+   * @var string
+   */
+  public $k_business = '';
+
+  /**
+   * Key of the user.
+   *
+   * @get get
+   * @var string
+   */
+  public $uid = '';
+}
+
+?>
