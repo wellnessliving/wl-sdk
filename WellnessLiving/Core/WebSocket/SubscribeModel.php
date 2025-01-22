0f68aa0a9795fe0d93efe194da4ce4712e853d5a
sdk.wellnessliving <integrations@wellnessliving.com>
2025-01-22 11:39:38 +0000

    Automatic upgrade of the SDK. Build #384511.

---
+++ WellnessLiving/Core/WebSocket/SubscribeModel.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace WellnessLiving\Core\WebSocket;
+
+use WellnessLiving\WlModelAbstract;
+
+/**
+ * Checks whether a listener can subscribe to specified channel.
+ *
+ * If yes, subscribes it.
+ */
+class SubscribeModel extends WlModelAbstract
+{
+  /**
+   * Keys of the channel.
+   *
+   * Key is name of a key field in a channel controller.
+   * Value is its value.
+   *
+   * Subscriber will only receive notifications about events in which all these values equal values specified here.
+   *
+   * @post post
+   * @var array
+   */
+  public $a_key = [];
+
+  /**
+   * All messages in queue. Key is a message key. Value is message data.
+   *
+   * When a client is initially subscribed for a WebSocket channel, entire message queue is sent to the client.
+   * This is needed to send message that were generated before a client has subscribed.
+   *
+   * @post result
+   * @var array
+   */
+  public $a_message_broadcast = [];
+
+  /**
+   * Channel controller CID.
+   *
+   * @post post
+   * @var int
+   */
+  public $cid_channel;
+
+  /**
+   * Subscriber password.
+   *
+   * @post post
+   * @var string
+   */
+  public $s_password;
+
+  /**
+   * Request token.
+   *
+   * This token is only used for asynchronous functions, and identifies a specific request.
+   *
+   * @post post
+   * @var string
+   */
+  public $s_token;
+}
+
+?>
--- WellnessLiving/WlModelRequest.php
+++ WellnessLiving/WlModelRequest.php
@@ -12,7 +12,7 @@ class WlModelRequest
   /**
    * SDK version number.
    */
-  const VERSION='20250121.383969';
+  const VERSION='20250122.384511';
 
   /**
    * A list of headers for the API request. See {@link CURLOPT_HTTPHEADER}.
