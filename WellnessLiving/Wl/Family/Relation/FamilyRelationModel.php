2f92617e49374fa03d620d494f3d715d8ec6d57b
sdk.wellnessliving <integrations@wellnessliving.com>
2025-02-20 15:54:16 +0000

    Automatic upgrade of the SDK. Build #391994.

---
+++ WellnessLiving/Wl/Family/Relation/FamilyRelationModel.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace WellnessLiving\Wl\Family\Relation;
+
+use WellnessLiving\WlModelAbstract;
+
+/**
+ * Returns business' enabled relationship types.
+ */
+class FamilyRelationModel extends WlModelAbstract
+{
+  /**
+   * The relationship types in the business.
+   *
+   * Key is ID, value is SID.
+   *
+   * @get result
+   * @var array
+   * @see WlFamilyRelationSid
+   */
+  public $a_business_relationships = [];
+
+  /**
+   * The business key.
+   *
+   * @get get
+   * @var string
+   */
+  public $k_business = '';
+}
+
+?>
--- WellnessLiving/WlModelRequest.php
+++ WellnessLiving/WlModelRequest.php
@@ -12,7 +12,7 @@ class WlModelRequest
   /**
    * SDK version number.
    */
-  const VERSION='20250220.391849';
+  const VERSION='20250220.391994';
 
   /**
    * A list of headers for the API request. See {@link CURLOPT_HTTPHEADER}.
