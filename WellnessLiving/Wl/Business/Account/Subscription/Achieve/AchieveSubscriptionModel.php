6adc0598cab08dcb5325cb2d98eb601adcda2f0d
sdk.wellnessliving <integrations@wellnessliving.com>
2025-10-09 08:42:51 +0000

    Automatic upgrade of the SDK. Build #432104.

---
+++ WellnessLiving/Wl/Business/Account/Subscription/Achieve/AchieveSubscriptionModel.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace WellnessLiving\Wl\Business\Account\Subscription\Achieve;
+
+use WellnessLiving\WlModelAbstract;
+
+/**
+ * Entry point to get information about business Achieve subscription.
+ */
+class AchieveSubscriptionModel extends WlModelAbstract
+{
+  /**
+   * If Achieve app has free subscription plan is `true`, otherwise (white label, professional) - `false`.
+   *
+   * @get result
+   * @var bool
+   */
+  public $is_free = false;
+
+  /**
+   * If Achieve app has white label subscription plan is `true`, otherwise (free, professional) - `false`.
+   *
+   * @get result
+   * @var bool
+   */
+  public $is_white_label = false;
+
+  /**
+   * Business key to check Achieve app subscription plan.
+   *
+   * @get get
+   * @var string
+   */
+  public $k_business = '0';
+}
+
+?>
--- WellnessLiving/WlModelRequest.php
+++ WellnessLiving/WlModelRequest.php
@@ -12,7 +12,7 @@ class WlModelRequest
   /**
    * SDK version number.
    */
-  const VERSION='20251009.432096';
+  const VERSION='20251009.432104';
 
   /**
    * A list of headers for the API request. See {@link CURLOPT_HTTPHEADER}.
