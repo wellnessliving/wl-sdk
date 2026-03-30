736effc8d7305a061aae5d6a6912814bc056fbcc
sdk.wellnessliving <integrations@wellnessliving.com>
2026-03-30 12:51:17 +0000

    Automatic upgrade of the SDK. Build #460415.

---
+++ WellnessLiving/Wl/Discount/Code/DiscountCodeModel.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace WellnessLiving\Wl\Discount\Code;
+
+use WellnessLiving\WlModelAbstract;
+
+/**
+ * Discount codes API point.
+ */
+class DiscountCodeModel extends WlModelAbstract
+{
+  /**
+   * Discount codes list.
+   *
+   * Each element has next structure:<dl>
+   * <dt>string <var>k_discount_code</var> </dt>
+   * <dd>Discount code key.</dd>
+   * <dt>string <var>text_discount_code</var></dt>
+   * <dd>Discount code title.</dd>
+   * </dl>
+   *
+   * @get result
+   * @var array[]
+   */
+  public $a_list;
+
+  /**
+   * Business key of the discount codes.
+   *
+   * @get get
+   * @var string
+   */
+  public $k_business;
+}
+
+?>
--- WellnessLiving/WlModelRequest.php
+++ WellnessLiving/WlModelRequest.php
@@ -12,7 +12,7 @@ class WlModelRequest
   /**
    * SDK version number.
    */
-  const VERSION='20260330.460388';
+  const VERSION='20260330.460415';
 
   /**
    * A list of headers for the API request. See {@link CURLOPT_HTTPHEADER}.
