78c5274ba738a7ead90177253cf4f43f13d5827d
sdk.wellnessliving <integrations@wellnessliving.com>
2026-04-02 15:44:56 +0000

    Automatic upgrade of the SDK. Build #461457.

--- WellnessLiving/WlModelRequest.php
+++ WellnessLiving/WlModelRequest.php
@@ -12,7 +12,7 @@ class WlModelRequest
   /**
    * SDK version number.
    */
-  const VERSION='20260402.461456';
+  const VERSION='20260402.461457';
 
   /**
    * A list of headers for the API request. See {@link CURLOPT_HTTPHEADER}.
